6e15332825535fd8968759a1d3fcc420257c1102
Mohammad Azeem Khan <khan.azeemak@gmail.com>
2025-06-26 19:16:45 +0200

    updated db.py

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ import psycopg
 from psycopg.rows import dict_row
 
 
-DATABASE_URL = {"host": "localhost", "dbname": "lib_db", "user": "postgres", "password": "password"}
+DATABASE_URL = {"host": "hostname", "dbname": "dbname", "user": "user", "password": "password"}
 
 def get_db_connection():
     conn = psycopg.connect(**DATABASE_URL, row_factory=dict_row)
